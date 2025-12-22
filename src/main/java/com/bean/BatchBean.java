a69f01dc6661398670df9a614c38a73c5d02336a
Tejas Shah <tejasshah2k19@gmail.com>
2025-12-22 13:01:05 +0530

    pattern ,min ,max validation

--- src/main/java/com/bean/BatchBean.java
+++ src/main/java/com/bean/BatchBean.java
@@ -1,18 +1,36 @@
 package com.bean;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public class BatchBean {
 
+//	@NotEmpty -- array 
+//	@Digits   -- 0-9
+//	@Email	  --  XX@XX 		XX@XX.XX
+//	@NegativeOrZero
+//	@Negative
+//	@Positive
+//	@PositiveOrZero
+
+	
+	
 	@NotBlank(message = "Please Enter Batch Name")
 	private String batchName;
-	
+
 	@NotBlank(message = "Please Enter Subject Name")
 	private String subjectName;
 
-	@NotBlank(message = "Please Enter Faculty Name")
+	@NotBlank(message = "Please Enter Faculty Valid Name")
+	@Pattern(regexp = "[a-zA-Z]+",message = "Please Enter Valid Faculty Name")
 	private String facultyName;
 
+	@Min(value = 2, message = "Hours should be greater or equals 2")
+	@Max(value = 5, message = "Max hours can be 5")
+	@NotNull(message = "Please Enter Total Hours")
 	private Integer totalHours;
 
 	public String getBatchName() {
